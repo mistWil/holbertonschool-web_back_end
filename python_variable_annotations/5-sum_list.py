ab8c7b9accd1048a08ef6a92cbbfbe2532f7415a
mistWil <7448@holbertonstudents.com>
2024-04-22 10:56:51 +0200

    5-sum_list.py

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+"""Complex types - list of floats"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """takes a list of floats as argument and returns their sum as a float"""
+    return sum(input_list)
