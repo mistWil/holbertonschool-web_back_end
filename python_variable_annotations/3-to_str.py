7d7ab22005d498eee20f9849c36597f292fcf28e
mistWil <7448@holbertonstudents.com>
2024-04-22 10:28:27 +0200

    3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+"""3. Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """takes float as argument returns the string of the float"""
+    return str(n)
