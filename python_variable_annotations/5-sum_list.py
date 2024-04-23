4e786f6a334a6c5c9aee6ad6f83cd6a4c2f1e385
mistWil <7448@holbertonstudents.com>
2024-04-23 13:33:34 +0200

    shebang changing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,9 +3,10 @@
 
 """Complex types - list of floats"""
 
+
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """takes a list of floats as argument and returns their sum as a float"""
     return sum(input_list)
