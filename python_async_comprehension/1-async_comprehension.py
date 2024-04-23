10c962e7a0250150d458456dffa1bc2b626f8dc2
mistWil <7448@holbertonstudents.com>
2024-04-23 13:42:24 +0200

    adding List import

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,10 +4,13 @@
 """Async Comprehensions"""
 
 
+from typing import List
+
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Collecte 10 nombres aléatoires en utilisant une async
     comprehension sur async_generator.
