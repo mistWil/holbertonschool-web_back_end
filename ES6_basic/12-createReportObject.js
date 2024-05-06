6b38d908cd076d8bc95b96a9fce6806f4239b2e7
mistWil <7448@holbertonstudents.com>
2024-05-06 11:50:12 +0200

    adding all tasks of the project1

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = { ...employeesList };
+  const getNumberOfDepartments = () => Object.keys(allEmployees).length;
+
+  return {
+    allEmployees,
+    getNumberOfDepartments,
+  };
+}
