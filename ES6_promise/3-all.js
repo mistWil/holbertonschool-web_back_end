a08710df844851a6219097f8b1dc52d282da6096
mistWil <7448@holbertonstudents.com>
2024-05-13 14:09:13 +0200

    3-all.js

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup () {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const [photoData, userData] = values;
+      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
